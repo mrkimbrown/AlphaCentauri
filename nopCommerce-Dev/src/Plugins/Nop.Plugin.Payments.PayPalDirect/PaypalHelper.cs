f41976183e1fb2b7ae5eccd7ce1e39a4c86a19d9
mrKimBrown <kym.brown@gmail.com>
2017-02-19 10:17:09 -0500

    Initial commit

---
+++ nopCommerce-Dev/src/Plugins/Nop.Plugin.Payments.PayPalDirect/PaypalHelper.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using PayPal.Api;
+
+namespace Nop.Plugin.Payments.PayPalDirect
+{
+    /// <summary>
+    /// Represents paypal helper
+    /// </summary>
+    public class PaypalHelper
+    {
+        /// <summary>
+        /// Get PayPal Api context 
+        /// </summary>
+        /// <param name="paypalDirectPaymentSettings">PayPalDirectPayment settings</param>
+        /// <returns>ApiContext</returns>
+        public static APIContext GetApiContext(PayPalDirectPaymentSettings payPalDirectPaymentSettings)
+        {
+            var mode = payPalDirectPaymentSettings.UseSandbox ? "sandbox" : "live";
+
+            var config = new Dictionary<string, string>
+            {
+                { "clientId", payPalDirectPaymentSettings.ClientId },
+                { "clientSecret", payPalDirectPaymentSettings.ClientSecret },
+                { "mode", mode }
+            };
+
+            var accessToken = new OAuthTokenCredential(config).GetAccessToken();
+            var apiContext = new APIContext(accessToken) { Config = config };
+
+            return apiContext;
+        }
+    }
+}
+
