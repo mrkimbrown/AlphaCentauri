7737fa7cbe253c81c19d6b193d62ae009630b022
mrKimBrown <kym.brown@gmail.com>
2017-03-12 20:59:10 -0400

    Customize CSS and text so that the main page will look more like the mockup

---
+++ nopCommerce-Dev/src/NOPCOMMERCE/dbo/PostDeployment.sql
@@ -0,0 +1,9 @@
+
+UPDATE [dbo].[LocaleStringResource]
+SET ResourceValue = '&nbsp'
+WHERE ResourceName = 'Search.Button'
+
+UPDATE [dbo].[LocaleStringResource]
+SET ResourceValue = 'Enter VIN or year, make and model'
+WHERE ResourceName = 'Search.SearchBox.Tooltip'
+
